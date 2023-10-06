e2fb94fd0e9014083e28b29ad3d49cbd6c569240
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-10-06 13:30:43 -0400

    add fonts and GitHub link

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,14 @@
-import colors from "tailwindcss/colors";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {},
+  theme: {
+    fontFamily: {
+      ...defaultTheme.fontFamily,
+      serif: ["Merriweather", ...defaultTheme.fontFamily.serif],
+      mono: ['"IBM Plex Mono"', ...defaultTheme.fontFamily.mono],
+    },
+  },
   plugins: [],
 };
